2a76bb5094433645d3f779967480ee4705d12ced
SAMKE-glitch <sammythemwa@gmail.com>
2023-09-24 03:23:31 -0700

    flask app

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Write a script that starts a Flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+"""
+
+from flask import Flask
+
+app = Flask("__name__")
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Returns a given string"""
+    return ("Hello HBNB!")
+
+
+@app.route("/HBNB", strict_slashes=False)
+def hbnb():
+    """Returns a given string"""
+    return ("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cText(text):
+    """ Displays "C" followed by the value of text"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythonText(text="is cool"):
+    """displays Python followed by the value of the variable text"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
